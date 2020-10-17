6f482a111058e0f708eaf257ce34dace6537a5eb
tisakg <tisakgz@gmail.com>
2020-10-17 10:21:41 -0500

    Tightly Coupled

---
+++ Spring-00-TightlyCoupled/src/CyberApp.java
@@ -0,0 +1,11 @@
+public class CyberApp {
+
+    public static void main(String[] args) {
+        FullTimeMentor fullTimeMentor =new FullTimeMentor();
+        PartTimeMentor partTimeMentor = new PartTimeMentor();
+
+        Mentor mentor = new Mentor(fullTimeMentor,partTimeMentor);
+
+        mentor.manageAccount();
+    }
+}
